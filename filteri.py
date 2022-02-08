5c5d1f494f2bdfd61ed3e35ae11f811bbad9d67f
Mahdi <95902320+Filtering-Rubika-Mahdi@users.noreply.github.com>
2022-02-08 14:55:29 +0330

    filteri.py

---
+++ filteri.py
@@ -0,0 +1,318 @@
+#mahdi heroristam
+#filtering
+print("")
+print("")
+print(" ")
+print("""
+#          #    "    m    m
+ mmmmm   mmm   # mm    mmm#  mmm    ##  ##
+ # # #  "   #  #"  #  #" "#    #    # ## #
+ # # #  m"""#  #   #  #   #    #    # "" #
+ # # #  "mm"#  #   #  "#m##  mm#mm  #    #""")
+print (" ")
+print("Load . . .")                 
+import os
+#What do you have in the source?
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+
+#data
+x = str(datetime.datetime.now())
+
+print("started " + (x))
+
+print(f"{blue} ")
+print ("      GOD FILTERING")
+print (Fore.YELLOW + "")
+x = """ Loading ~ ~ ~ 
+
+
+
+
+🖤🖤🖤🖤🖤🖤🖤🖤🖤🖤🖤🖤🖤🖤🖤🖤🖤
+🖤🖤🖤🖤🖤🖤🖤🖤🖤🖤🖤🖤🖤
+🖤🖤🖤🖤🖤🖤🖤🖤🖤🖤🖤
+🖤🖤🖤🖤🖤🖤🖤🖤🖤
+💙💙💙💙💙💙💙💙
+💔💔💔💔💔💔
+😹😹😹😹
+✋✋✋
+👍👍
+🧑‍🦯
+
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+
+print(f"{red} ")
+d = ("""
+
+m""    "    ""#      m                                       #        "
+ mm#mm  mmm      #    mm#mm   mmm    m mm          m mm  m   m  #mmm   mmm
+   #      #      #      #    #"  #   #"  "         #"  " #   #  #" "#    #
+   #      #      #      #    #""""   #             #     #   #  #   #    #
+   #    mm#mm    "mm    "mm  "#mm"   #             #     "mm"#  ##m#"  mm#mm
+
+
+
+
+
+
+ #
+ #   m   mmm
+ # m"   "   #
+ #"#    m"""#
+ #  "m  "mm"#
+
+                  god filteri
+                    CODE FILTERI RUBIKA
+
+
+
+▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎
+■■■■■■■■■■■■■
+◇♡♤■●¤¤¤▪︎》《♡♡♤□●●|○●
+°•```《°《°¡°¡°¡°¡•《•《•《`♤♤`¡`
+¿◇¡●《《○♡○♡○♡♡●《●♤●
+         ,,,,****×,,×*×)×**÷■■""")
+
+for d in d:
+        sys.stdout.write(d)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+# Chaker * _ *  Hal Kardi?
+
+time.sleep(4.3)
+print(f"{green} ")
+print ("Play - - - ! ")
+print (Fore.YELLOW + "")
+time.sleep(1.7)
+print ("insta @creator_tor")
+print(f"{blue} ")
+time.sleep(1.0)
+print ("!!")
+time.sleep(0.7)
+print ("lod....")
+time.sleep(0.7)
+print ("n/y")
+time.sleep(0.6)
+print ("....")
+time.sleep(0.5)
+print ("......")
+time.sleep(0.4)
+print (".........")
+time.sleep(0.3)
+print ("..................")
+time.sleep(0.2)
+print (".........................")
+time.sleep(0.1)
+print (".............................")
+time.sleep(0.5)
+print ("...............................")
+time.sleep(0.1)
+print (".................................")
+time.sleep(0.5)
+print ("....................................")
+time.sleep(0.1)
+print ("~iegn")
+time.sleep(0.5)
+print ("cood info server")
+time.sleep(0.1)
+print ("script server cood")
+time.sleep(0.5)
+print ("server1:/kgj/af0////")
+time.sleep(1.0)
+print ("server2:/e/f.g////")
+time.sleep(0.5)
+print ("server3:/f////g.h.")
+time.sleep(0.1)
+print ("server4:d//h/f")
+time.sleep(0.5)
+print ("server5:g0/k/j/a/f.")
+time.sleep(1.0)
+print ("making script mahdi")
+time.sleep(0.5)
+print ("script making cood filter rubika.apk 2.9.5 80%")
+time.sleep(1.0)
+print ("script server")
+time.sleep(0.5)
+print ("script connect server rubika.ir/rubika.com/ file os server")
+time.sleep(1.0)
+print ("cood filter group 90% apn cood proxy-server")
+time.sleep(0.5)
+print ("cood filter account rubika 87% apn cood filter proxy-port:8000")
+time.sleep(1.0)
+print ("cood filter chaneel rubika 78% apn proxy file port:auth")
+time.sleep(0.5)
+print ("")
+print (" ")
+print ("")
+# 1 chanel 2 Group 3 acc
+up = input(""" 
+
+[cood filter channel rubika (1) ]
+[cood filtr group rubika    (2) ]
+
+[cood filter Account rubika (3) ]
+
+[Super   (0) ]
+
+[Exit    (4) ]
+
+[report aut (5) ]
+
+[Code Rp (6) ]
+
+[ account on filter (7) ]
+
+Which do you want>>>""")
+print("")
+print(f"{pink} ")
+time.sleep(3.0)
+print(""" in the name of God 
+_____________
+
+Loading . . . .
+
+. . . . . . .  . .
+
+ . . . . . . . . . . 
+
+💣 💣 💣 💣 💣 💣 💣 💣
+
+
+
+|||||Telegram: Mahdim1200 |||| 
+|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|
+____________ id:hacker_mahdi___________
+""")
+print(f"{red} ")
+
+if up == "7":
+    os.system("python sms.py")
+if up == "6":
+    os.system("python report.py")
+if up == "5":
+    os.system("python pass.py")
+if up == "4":
+    os.system("exit 5")
+
+if up == "0":
+         print ('(Super) Code filter =>' + '%s%s  (/5.6.7.filtering-rubika.ir/*+³⁶///6.5.9.8.h//310.561///5.1.1.6) %s' % (fg('red'), bg('black'), attr('reset')))
+
+
+if up == "1":
+
+        print ('code Fil Channel ≠>' + '%s%s (7.8.9.2/kgj/af0/////6065///g.k.4.6.7.8.2.3.5.6.7.3.2.8.3.9.1.5.3.6.2.9.6/) %s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+if up != "1":
+        print("    COD  ")
+
+
+if up == "2":
+        print ('code group ≠>' + '%s%s (4.0.1.4/f////g.h.4.5.3//5.2.5.7.2.6.8.4.2.4.8.9.3.1.5.2.7.9.8.0.1.3) %s' % (fg('white'), bg('black'), attr('reset')))
+
+
+if up != "2":
+
+        print("     cσ∂s")
+
+
+if up == "3":
+
+        print ('code Filter account ≠>' + '%s%s (6.0.3.6/d//h/f.6.0.3.2.3.8.6.4.5.8.6.3.7.7.9.0.4.5.1.2.4.0.3.4) %s' % (fg('red'), bg('blue'), attr('reset')))
+
+
+if up != "3":
+        print("""
+
+             Khalegh rubik""")
+
+time.sleep(0.6)
+print(f"{green}" )
+print ("""mahdi""")
+print(f"{blue}" )
+print(" ")
+print("ip rubika.ir = 5.106.10.226 ")
+print("")
+print(" Chanel Telegram=>> mahdi -")
+print (Fore.WHITE + " ")
+time.sleep(3.0)
+print (" Ostoreh Filtering-hacker All mahdi")
+time.sleep(3.0)
+print (f"{red} ")
+time.sleep(1)
+print ('Cod Ro Dorost Bezan 70% Filter Mikone  ' + '%s%s TanK You My Friend  %s'  % (fg('red'), bg('white'), attr('reset')))
+print (f"{blue} ")
+time.sleep(1)
+print (""" {code ro ( 5 ) bar bezanin to Sayer!!}
+baraye account Tanks""")
+print(" ")
+print(" ")
+print(" ")
+print ("          THE END!")
+print("")
+print(""" (Update Kardan script) 👇:
+
+rm -rf mahdi.1382
+
+
+git clone https://github.com/Filtering-Rubika-Mahdi/mahdi.1382
+
+
+cd mahdi.1382
+
+
+python filteri.py
+
+""")
+print("")
+print("")
+print("         </mahdi>")
+print ("")
+time.sleep(0.3)
+print(f"{red}" )
+time.sleep(1)
+print ("""ostorh filteri""")
+print("")
+print (Fore.WHITE + "")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
